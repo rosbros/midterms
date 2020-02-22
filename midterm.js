903c90915cd69fd79411c6b43590bed842d25e2b
Vadlit <57819558+rosbros@users.noreply.github.com>
2020-02-22 15:56:22 +0800

    Add files via upload

---
+++ midterm.js
@@ -0,0 +1,34 @@
+var position=0;
+$(document).ready(function(){
+  $(document).keydown(function(e){
+      switch(e.which){
+           case 37:
+           pos_less_1();
+           break;
+           case 39:
+           pos_plus_1();
+           break;
+       }
+       apply_pos();
+  });
+});
+
+function pos_less_1() {
+  position--;
+  if(position < 0) {
+  position=-(position% 3);
+  }
+ }
+
+function pos_plus_1() {
+  position++;
+  if(position > 2){
+  position=(position% 3);
+  }
+}
+
+function apply_pos(){
+ var c=$("#container");
+ c.removeClass();
+ c.addClass("move"+position);
+}
